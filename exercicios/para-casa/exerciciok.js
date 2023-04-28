2f2d7e33bc854f4388bcd90ef466001f931e2d6c
Kai <kathleentsribeiro9@gmail.com>
2023-04-27 23:00:20 -0300

    meu exercicio de js

---
+++ exercicios/para-casa/exerciciok.js
@@ -0,0 +1,8 @@
+let nota1 = parseInt(prompt('Nota1'))
+let nota2 = parseInt(prompt('Nota2'))
+let media = (nota1 + nota2) / 2
+
+if (media <= 7) {
+  alert('Você não passou, relaxa porque tem recuperação.')
+} else media >= 5
+alert('Parabéns, você foi aprovado!')
